0d8c7952531a1c2351105e2e0078a80b4d8040e0
Blake Hutchinson <blake.hutchinson.1996@gmail.com>
2026-05-19 15:20:45 -0400

    Register component added

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,17 +1,25 @@
 // import React from 'react'
 import './assets/css/style.css'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Main from './components/Main'
-import Footer from './components/Footer'
+import Register from './components/Register'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+
     </>
   )
 }
