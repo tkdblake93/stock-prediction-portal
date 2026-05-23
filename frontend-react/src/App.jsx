9e1c3912cb1ccbcfdc5b0ae5cd4c729d80069d6d
Blake Hutchinson <blake.hutchinson.1996@gmail.com>
2026-05-23 07:42:41 -0400

    Authentication functionalities added

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,12 +1,15 @@
-// import React from 'react'
+import { useState } from 'react'
 import './assets/css/style.css'
 import AuthProvider from './AuthProvider'
+import Dashboard from './components/dashboard/Dashboard'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Login from './components/Login'
 import Main from './components/Main'
 import Register from './components/Register'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
 
 
 function App() {
@@ -18,8 +21,9 @@ function App() {
           <Header />
           <Routes>
             <Route path='/' element={<Main />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<PublicRoute><Register /></PublicRoute>} />
+            <Route path='/login' element={<PublicRoute><Login /></PublicRoute>} />
+            <Route path='/dashboard' element={<PrivateRoute><Dashboard /></PrivateRoute>} />
           </Routes>
           <Footer />
         </BrowserRouter>
---
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+import axiosInstance from '../../axiosInstance'
+
+const Dashboard = () => {
+  useEffect(() => {
+    const fetchProtectedData = async () => {
+      try {
+        const response = await axiosInstance.get('/protected-view/')
+        console.log('Success: ', response.data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+    fetchProtectedData()
+  }, [])
+  return (
+    <div className='text-light container'>Dashboard</div>
+  )
+}
+
+export default Dashboard
